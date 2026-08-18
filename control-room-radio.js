d3c192e76e91b3ae9ab610cbbacd93ddb578cfa4
Curiositydrop <info@curiositydrop.com>
2026-08-18 11:10:07 -0400

    Fix radio admin schedule list and add Up Next controls
    * Stop legacy schedule display race
    
    * Add admin Up Next wording controls
    
    * Use configurable Up Next wording
    
    * Refresh radio control assets
    
    * Refresh radio control assets
    
    * Refresh radio control assets

--- control-room-radio.js
+++ control-room-radio.js
@@ -1,3 +1,3 @@
-import './control-room-radio-v2.js?v=5';
-import './radio-playlist-save-fix.js?v=9';
+import './control-room-radio-v2.js?v=6';
+import './radio-playlist-save-fix.js?v=10';
 import './radio-admin-playlist-tools.js?v=1';
