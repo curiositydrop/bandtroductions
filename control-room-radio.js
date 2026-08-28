c69cfc503907f1cda0c70aee4977505f79bf52ef
Curiositydrop <info@curiositydrop.com>
2026-08-28 00:57:38 -0400

    Add mobile playlist reorder and shuffle

--- control-room-radio.js
+++ control-room-radio.js
@@ -1,3 +1,3 @@
-import './control-room-radio-v2.js?v=6';
-import './radio-playlist-save-fix.js?v=10';
+import './control-room-radio-v2.js?v=7';
+import './radio-playlist-save-fix.js?v=11';
 import './radio-admin-playlist-tools.js?v=1';
